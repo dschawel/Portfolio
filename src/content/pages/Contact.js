851bc9aeaf8b44ead8c95fc5f2a90a7447649c2e
dschawel <david.schawel@gmail.com>
2021-02-09 13:52:05 -0800

    added ubb to projects and re-did the home page

--- src/content/pages/Contact.js
+++ src/content/pages/Contact.js
@@ -10,25 +10,25 @@ const Contact = props => {
             <h1>Feel free to contact me!</h1>
             <div className="icons">
                 <a href="https://www.linkedin.com/in/davidschawel/">
-                    <FontAwesomeIcon className='font-awesome' icon={faLinkedin} size = '3x'/>
+                    <FontAwesomeIcon className='font-awesome' icon={faLinkedin} size='3x' />
                     <p className="icon"><strong>LinkedIn</strong></p>
                 </a>
             </div>
             <div className="icons">
                 <a href="https://github.com/dschawel">
-                    <FontAwesomeIcon className='font-awesome' icon={faGithub} size = '3x'/>
+                    <FontAwesomeIcon className='font-awesome' icon={faGithub} size='3x' />
                     <p className="icon"><strong>GitHub</strong></p>
                 </a>
             </div>
             <div className="icons">
                 <a href="mailto:david.schawel@gmail.com">
-                    <FontAwesomeIcon className='font-awesome' icon={faEnvelope} size = '3x'/>
+                    <FontAwesomeIcon className='font-awesome' icon={faEnvelope} size='3x' />
                     <p className="icon"><strong>Email</strong></p>
                 </a>
             </div>
             <div className="icons">
                 <a href="/David_Schawel_Resume.pdf" target="_blank">
-                    <FontAwesomeIcon className='font-awesome' icon={faFile} size = '3x'/>
+                    <FontAwesomeIcon className='font-awesome' icon={faFile} size='3x' />
                     <p className="icon"><strong>Resume</strong></p>
                 </a>
             </div>
